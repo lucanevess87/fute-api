c0858e87620ae59a5e0e49d77bab9f31132443f9
lucaneves87 <lucatets@gmail.com>
2023-09-18 23:43:55 -0300

    fix: import

--- tests/util/createAndAuthenticateUser.ts
+++ tests/util/createAndAuthenticateUser.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import { Express } from 'express';
-import { hash } from 'bcryptjs';
+import bcryptjs from 'bcryptjs';
 import prisma from '../../src/database/client';
 
 export async function createAndAuthenticateUser(app: Express) {
@@ -8,17 +8,15 @@ export async function createAndAuthenticateUser(app: Express) {
     data: {
       name: 'John Doe',
       email: 'johndoe@example.com',
-      password: await hash('123456', 6),
+      password: await bcryptjs.hash('123456', 6),
     },
   });
 
-  const tokenResponse = await request(app)
-    .post('/sessions')
-    .send({
-      email: 'johndoe@example.com',
-      password: '123456',
-    });
-  
+  const tokenResponse = await request(app).post('/sessions').send({
+    email: 'johndoe@example.com',
+    password: '123456',
+  });
+
   const { id } = user;
 
   const { accessToken: token } = tokenResponse.body.data;
